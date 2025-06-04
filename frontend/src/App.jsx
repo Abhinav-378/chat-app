f91d752dbdc7d18a0a2a404dd796fff6dd0be762
Abhinav-378 <abhichoukse05@gmail.com>
2025-06-04 13:47:21 +0530

    restructure App component for routing and user authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
+import ChatPage from './components/ChatPage.jsx';
 function App() {
-  const [count, setCount] = useState(0)
-
+  const user = JSON.parse(localStorage.getItem('user'));
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={user ? <ChatPage /> : <Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
